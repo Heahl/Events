9d55358c9e9a0de51e1158663b64a5e7b8ffb93e
heahl <hendrik712.a@gmail.com>
2026-01-14 09:37:25 +0100

    hotfix register

--- public/javascripts/auth/register.js
+++ public/javascripts/auth/register.js
@@ -13,7 +13,8 @@ async function registerForm() {
             const response = await fetch('/register', {
                 method: 'POST',
                 headers: {
-                    'Content-Type': 'application/json'
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
                 },
                 body: JSON.stringify(data)
             });
